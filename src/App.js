f9565b062e17505fbabd6536477c33c3fcfc8060
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-09-01 18:29:40 -0400

    Skeleton for login

--- src/App.js
+++ src/App.js
@@ -1,12 +1,38 @@
-import './App.css';
-import Todos from './Todos';
+import React, { useState } from "react";
+import AccountCircle from "@material-ui/icons/AccountCircle";
+import LockIcon from "@material-ui/icons/Lock";
+import "./styles/App.css";
+import Todos from "./Todos";
 
 function App() {
-  return (
-    <div>
-      <Todos/>
-    </div>
-  );
+	return (
+		<div>
+			<form>
+				<div>
+					<label>Email:</label>
+					<br />
+					<span>
+						<AccountCircle />
+					</span>
+					<input type="email" name="email" placeholder="user@rapptrlabs.com" />
+				</div>
+				<div>
+					<label>Password:</label>
+					<br />
+					<span>
+						<LockIcon />
+					</span>
+					<input
+						type="password"
+						name="password"
+						placeholder="Must be at least 4 characters"
+					/>
+				</div>
+				<button type="submit">Login</button>
+			</form>
+			<Todos />
+		</div>
+	);
 }
 
 export default App;
