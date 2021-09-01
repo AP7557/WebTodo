06f754c6949d63aa025058adcc1c0da91eadf3b4
Akash Patel <ap.akash.patel1999+github@gmail.com>
2021-09-01 18:35:34 -0400

    Store the user and render/derender login/todolist based on login

--- src/App.js
+++ src/App.js
@@ -5,32 +5,63 @@ import "./styles/App.css";
 import Todos from "./Todos";
 
 function App() {
+	const [user, setUser] = useState({ email: "", password: "" });
+	const [isUserActive, setIsUserActive] = useState(false);
+	const handleEmailChange = (e) => {
+		setUser((prevState) => {
+			return { ...prevState, email: e.target.value };
+		});
+	};
+	//On every password input change
+	const handlePasswordChange = (e) => {
+		setUser((prevState) => {
+			return { ...prevState, password: e.target.value };
+		});
+	};
+	const onLogin = (e) => {
+		e.preventDefault();
+		setIsUserActive(true);
+	};
+	const onLogout = () => {
+		setUser({ email: "", password: "" });
+		setIsUserActive(false);
+	};
 	return (
 		<div>
-			<form>
-				<div>
-					<label>Email:</label>
-					<br />
-					<span>
-						<AccountCircle />
-					</span>
-					<input type="email" name="email" placeholder="user@rapptrlabs.com" />
-				</div>
-				<div>
-					<label>Password:</label>
-					<br />
-					<span>
-						<LockIcon />
-					</span>
-					<input
-						type="password"
-						name="password"
-						placeholder="Must be at least 4 characters"
-					/>
-				</div>
-				<button type="submit">Login</button>
-			</form>
-			<Todos />
+			{isUserActive ? (
+				<Todos email={user.email} />
+			) : (
+				<>
+					<h2>Rapptr Labs</h2>
+					<form>
+						<div>
+							<label>Email:</label>
+							<br />
+							<span>
+								<AccountCircle />
+							</span>
+							<input
+								type="email"
+								name="email"
+								placeholder="user@rapptrlabs.com"
+							/>
+						</div>
+						<div>
+							<label>Password:</label>
+							<br />
+							<span>
+								<LockIcon />
+							</span>
+							<input
+								type="password"
+								name="password"
+								placeholder="Must be at least 4 characters"
+							/>
+						</div>
+						<button type="submit">Login</button>
+					</form>
+				</>
+			)}
 		</div>
 	);
 }
